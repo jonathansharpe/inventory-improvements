c3f952011cbfc7ca0613e2af5f3d5d0b807b9cd2
jonathansharpe <jonathansharpe54@gmail.com>
2024-06-23 12:59:48 -0700

    pushing initial files

---
+++ src/client/java/jsharpe/ii/InventoryImprovementsClient.java
@@ -0,0 +1,10 @@
+package jsharpe.ii;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InventoryImprovementsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
